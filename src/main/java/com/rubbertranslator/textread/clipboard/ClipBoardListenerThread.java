1eee6018ab487a818dd71f22f314643d4aa8069a
Raven <zhang.xingrui@foxmail.com>
2020-05-07 09:44:04 +0800

    ClipboardListener scheme v2: use lostOwnerListener and while loop to get text from clipboard
    This scheme is better than v1. And we change the wait time dynamically. But there is a problem need to be fixed: we waste too much cpu time.

--- src/main/java/com/rubbertranslator/textread/clipboard/ClipBoardListenerThread.java
+++ src/main/java/com/rubbertranslator/textread/clipboard/ClipBoardListenerThread.java
@@ -1,52 +1,86 @@
 package com.rubbertranslator.textread.clipboard;
 
 import java.awt.*;
-import java.awt.datatransfer.*;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
 
 
-public class ClipBoardListenerThread extends Thread {
+public class ClipBoardListenerThread extends Thread implements ClipboardOwner {
+    // 系统剪切板
+    private final Clipboard sysClip = Toolkit.getDefaultToolkit().getSystemClipboard();
+    // fence用于线程保活
+    private final Fence fence = new Fence();
+    // 动态等待时间
+    private long waitTime = 50;
 
-    // 程序终止
-    private volatile boolean exit = false;
-
-    // 程序暂停
-    private volatile boolean running = true;
-    private Object blocker = new Object();
+    public ClipBoardListenerThread(){
+        Transferable trans = sysClip.getContents(this);
+        sysClip.setContents(trans, this);
+    }
 
     @Override
     public void run() {
-        String initial = "";
-        while (!exit) {
+        fence.keepAlive();
+    }
+
+    @Override
+    public void lostOwnership(Clipboard c, Transferable t) {
+        Transferable contents;
+        // 循环require clipboard owner
+        boolean required = false;
+        final long minWaitTime = 50;
+        final long maxWaitTime = 3000;
+        // 动态等待时间
+        while(!required){
             try {
-                // 怎么避免浪费CPU时间？
-                Thread.sleep(200);
-                if (!running) blocker.wait();
-                Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
-                String paste = c.getContents(null).getTransferData(DataFlavor.stringFlavor).toString();
-                if (!paste.equals(initial)) {
-                    System.out.println(paste);
-                    initial = paste;
+                //waiting e.g for loading huge elements like word's etc.
+                Thread.sleep(waitTime);
+                contents = sysClip.getContents(null);
+                sysClip.setContents(t, this);
+                required = true;
+                processClipboard(contents, c);
+            } catch (InterruptedException | IllegalStateException e) {
+                if(waitTime < maxWaitTime){
+                    waitTime+=100;  // 增加100ms等待时间
                 }
-            } catch (InterruptedException | UnsupportedFlavorException | IllegalStateException | IOException ex) {
-                Logger.getLogger(ClipBoardListenerThread.class.getName()).log(Level.SEVERE, null, ex);
+                e.printStackTrace();
+            }finally {
+                if(waitTime > minWaitTime){
+                    waitTime-=10;   // 减少10ms等待时间
+                }
             }
+        }
+
+    }
+
+    public void processClipboard(Transferable t, Clipboard c) { //your implementation
+        try {
+            if (t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                String tempText = (String) t.getTransferData(DataFlavor.stringFlavor);
+                System.out.println(tempText);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 
-    public void pause() {
-        running = false;
-    }
 
-    public void resumeRun() {
-        running = true;
-        blocker.notify();
-    }
+    /**
+     * 阻塞器，用于线程保活
+     */
+    private static class Fence{
+        public synchronized void keepAlive(){
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
-    public void exit() {
-        exit = true;
+        public synchronized void dead(){
+            this.notify();
+        }
     }
-
 }
