3f1fa44c8ba14acabed3144d1bde55df8fc089ab
Raven <zhang.xingrui@foxmail.com>
2020-05-07 09:22:41 +0800

    ClipboardListener scheme v1: polling clipboard.

--- src/main/java/com/rubbertranslator/textread/clipboard/ClipBoardListenerThread.java
+++ src/main/java/com/rubbertranslator/textread/clipboard/ClipBoardListenerThread.java
@@ -1,70 +1,52 @@
 package com.rubbertranslator.textread.clipboard;
 
 import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.ClipboardOwner;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.*;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
-public class ClipBoardListenerThread extends Thread implements ClipboardOwner {
-    private final Clipboard sysClip = Toolkit.getDefaultToolkit().getSystemClipboard();
-    private final Fence fence = new Fence();
+public class ClipBoardListenerThread extends Thread {
 
-    public ClipBoardListenerThread(){
-        Transferable trans = sysClip.getContents(this);
-        sysClip.setContents(trans, this);
-    }
+    // 程序终止
+    private volatile boolean exit = false;
+
+    // 程序暂停
+    private volatile boolean running = true;
+    private Object blocker = new Object();
 
     @Override
     public void run() {
-        fence.block();
-    }
-
-    @Override
-    public void lostOwnership(Clipboard c, Transferable t) {
-        System.out.println(Thread.currentThread());
-        Transferable contents = null;
-        try {
-            Thread.sleep(200);  //waiting e.g for loading huge elements like word's etc.
-            contents = sysClip.getContents(this);
-            processClipboard(contents, c);
-            sysClip.setContents(t, this);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }catch (IllegalStateException e){
-            // 剪贴板监听线程出现异常，重新启动一个
-            System.out.println("异常");
-            fence.unblock();
-        }
-    }
-
-    public void processClipboard(Transferable t, Clipboard c) { //your implementation
-        try {
-            if (t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
-                String tempText = (String) t.getTransferData(DataFlavor.stringFlavor);
-                System.out.println(tempText);
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-
-    /**
-     * 阻塞器，用于线程保活
-     */
-    private static class Fence{
-        public synchronized void block(){
+        String initial = "";
+        while (!exit) {
             try {
-                this.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                // 怎么避免浪费CPU时间？
+                Thread.sleep(200);
+                if (!running) blocker.wait();
+                Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
+                String paste = c.getContents(null).getTransferData(DataFlavor.stringFlavor).toString();
+                if (!paste.equals(initial)) {
+                    System.out.println(paste);
+                    initial = paste;
+                }
+            } catch (InterruptedException | UnsupportedFlavorException | IllegalStateException | IOException ex) {
+                Logger.getLogger(ClipBoardListenerThread.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-
-        public synchronized void unblock(){
-            this.notify();
-        }
     }
+
+    public void pause() {
+        running = false;
+    }
+
+    public void resumeRun() {
+        running = true;
+        blocker.notify();
+    }
+
+    public void exit() {
+        exit = true;
+    }
+
 }
