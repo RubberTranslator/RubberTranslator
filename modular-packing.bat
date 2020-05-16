0448fddffa6c895f1eda0f74b5118d9ad606cd8a
Raven <zhang.xingrui@foxmail.com>
2020-05-16 22:36:19 +0800

    Try to modularize

---
+++ modular-packing.bat
@@ -0,0 +1,5 @@
+jpackage --name "RubberTranslator" ^
+        --dest target ^
+        --type app-image ^
+        --module-path C:\Users\Raven\IdeaProjects\RubberTranslator\target\RubberTranslator-1.0-SNAPSHOT.jar;C:\Users\Raven\.m2\repository\cglib\cglib\3.3.0\cglib-3.3.0.jar;C:\Users\Raven\.m2\repository\com\1stleg\jnativehook\2.0.2\jnativehook-2.0.2.jar;C:\Users\Raven\.m2\repository\com\google\code\gson\gson\2.8.6\gson-2.8.6.jar;C:\Users\Raven\.m2\repository\com\squareup\okhttp3\okhttp\3.6.0\okhttp-3.6.0.jar;C:\Users\Raven\.m2\repository\net\java\dev\jna\jna\4.0.0\jna-4.0.0.jar;C:\Users\Raven\.m2\repository\net\java\dev\jna\jna-platform\4.0.0\jna-platform-4.0.0.jar;C:\Users\Raven\.m2\repository\org\openjfx\javafx-base\14\javafx-base-14-win.jar;C:\Users\Raven\.m2\repository\org\openjfx\javafx-controls\14\javafx-controls-14-win.jar;C:\Users\Raven\.m2\repository\org\openjfx\javafx-fxml\14\javafx-fxml-14-win.jar;C:\Users\Raven\.m2\repository\org\openjfx\javafx-graphics\14\javafx-graphics-14-win.jar;C:\Users\Raven\.m2\repository\org\ow2\asm\asm\8.0.1\asm-8.0.1.jar ^
+         -m rubbertranslator/com.rubbertranslator.App --vendor raven
---
+++ src/main/resources/module-info(back-up-only).java
@@ -0,0 +1,30 @@
+/**
+ * @author Raven
+ * date  2020/5/16 19:24
+ * module-info文件目前无效，因为以来了non-module的jar包，所以没办法使用jlink打包
+ */
+open module rubbertranslator {
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+    requires java.logging;
+    requires com.google.gson;
+    requires jna;
+    requires jna.platform;
+    requires org.objectweb.asm;
+    requires cglib;
+    requires jnativehook;
+    requires okhttp3;
+
+    exports com.rubbertranslator;
+    exports com.rubbertranslator.modules.textinput;
+    exports com.rubbertranslator.modules.filter;
+    exports com.rubbertranslator.modules.textprocessor.pre;
+    exports com.rubbertranslator.modules.textprocessor.post;
+    exports com.rubbertranslator.modules.translate;
+    exports com.rubbertranslator.modules.history;
+    exports com.rubbertranslator.modules.afterprocess;
+    exports com.rubbertranslator.modules.system;
+}
