e354ae7da012c29d6af694e37569571bb68c95f7
Raven <zhang.xingrui@foxmail.com>
2020-12-23 22:53:01 +0800

    删除junique保护库

--- src/main/java/com/rubbertranslator/utils/AppSingletonUtil.java
+++ src/main/java/com/rubbertranslator/utils/AppSingletonUtil.java
@@ -1,8 +1,5 @@
 package com.rubbertranslator.utils;
 
-import it.sauronsoftware.junique.AlreadyLockedException;
-import it.sauronsoftware.junique.JUnique;
-
 /**
  * @author Raven
  * @version 1.0
@@ -10,13 +7,14 @@ import it.sauronsoftware.junique.JUnique;
  */
 public class AppSingletonUtil {
     public static boolean isAppRunning(){
-        boolean alreadyRunning;
-        try {
-            JUnique.acquireLock("RubberTranslator");
-            alreadyRunning = false;
-        } catch (AlreadyLockedException e) {
-            alreadyRunning = true;
-        }
-        return alreadyRunning;
+//        boolean alreadyRunning;
+//        try {
+//            JUnique.acquireLock("RubberTranslator");
+//            alreadyRunning = false;
+//        } catch (AlreadyLockedException e) {
+//            alreadyRunning = true;
+//        }
+//        return alreadyRunning;
+        return false;
     }
 }
--- src/main/java/com/rubbertranslator/utils/UpdateUtils.java
+++ src/main/java/com/rubbertranslator/utils/UpdateUtils.java
@@ -26,7 +26,8 @@ public class UpdateUtils {
 
         // get remote version
         Request request = new Request.Builder()
-                .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+                .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+                .addHeader("Content-Type","text/plain; charset=utf-8")
                 .url(versionUrl)
                 .build();
 
