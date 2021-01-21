8a60e3eedbd3390aa69b73e3a23caefd87b72140
raven <zhang.xingrui@foxmail.com>
2021-01-21 19:07:53 +0800

    Fix bug: linux file chooser crash

--- Main/src/main/java/com/rubbertranslator/mvp/modules/textinput/mousecopy/listener/GlobalMouseListener.java
+++ Main/src/main/java/com/rubbertranslator/mvp/modules/textinput/mousecopy/listener/GlobalMouseListener.java
@@ -1,8 +1,10 @@
 package com.rubbertranslator.mvp.modules.textinput.mousecopy.listener;
 
 import com.rubbertranslator.mvp.modules.textinput.mousecopy.copymethods.MouseEventDispatcher;
-import org.jnativehook.mouse.NativeMouseAdapter;
+import org.jnativehook.GlobalScreen;
 import org.jnativehook.mouse.NativeMouseEvent;
+import org.jnativehook.mouse.NativeMouseInputListener;
+import org.jnativehook.mouse.NativeMouseMotionListener;
 
 /**
  * @author Raven
@@ -11,7 +13,7 @@ import org.jnativehook.mouse.NativeMouseEvent;
  * 全局mouse event监听
  * 鼠标复制事件分发，采用责任链分发
  */
-public class GlobalMouseListener extends NativeMouseAdapter {
+public class GlobalMouseListener implements NativeMouseInputListener {
 
     private final MouseEventDispatcher mouseEventDispatcher = new MouseEventDispatcher();
     private boolean needDispatch = true;
@@ -24,17 +26,32 @@ public class GlobalMouseListener extends NativeMouseAdapter {
         this.needDispatch = needDispatch;
     }
 
+
     @Override
-    public void nativeMousePressed(NativeMouseEvent nativeEvent) {
-        if(needDispatch){
-            mouseEventDispatcher.pressEventDispatch(nativeEvent);
+    public void nativeMouseClicked(NativeMouseEvent nativeMouseEvent) {
+    }
+
+    @Override
+    public void nativeMousePressed(NativeMouseEvent nativeMouseEvent) {
+        if (needDispatch) {
+            mouseEventDispatcher.pressEventDispatch(nativeMouseEvent);
         }
     }
 
     @Override
-    public void nativeMouseReleased(NativeMouseEvent nativeEvent) {
-        if(needDispatch){
-            mouseEventDispatcher.releaseEventDispatch(nativeEvent);
+    public void nativeMouseReleased(NativeMouseEvent nativeMouseEvent) {
+        if (needDispatch) {
+            mouseEventDispatcher.releaseEventDispatch(nativeMouseEvent);
         }
+    }
+
+    @Override
+    public void nativeMouseMoved(NativeMouseEvent nativeMouseEvent) {
+
+    }
+
+    @Override
+    public void nativeMouseDragged(NativeMouseEvent nativeMouseEvent) {
+
     }
 }
