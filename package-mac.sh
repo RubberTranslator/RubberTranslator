575aaa78180fcd92f6f61600745bc512fb8a6522
ravenxrz <zhang.xingrui@foxmail.com>
2021-02-03 13:26:33 +0800

    添加mac需知

--- package-mac.sh
+++ package-mac.sh
@@ -4,7 +4,6 @@
 # 1. 分别在Launcher和Main目录下执行 mvn jfx:native
 # 2. 然后运行本脚本
 #
-#
 
 # 拷贝Main模块到Launcher模块
 cp -rf ./Main/target/jfx/native/Main.app/Contents/MacOS/Main ./Launcher/target/jfx/native/RubberTranslator.app/Contents/MacOS/
