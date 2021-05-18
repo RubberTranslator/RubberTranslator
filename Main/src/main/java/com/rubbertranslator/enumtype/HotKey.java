6e7ca15eab87ad87a501bc624151c973c347c261
ravenxrz <zhang.xingrui@foxmail.com>
2021-05-18 22:10:57 +0800

    Upload KeyCode

--- Main/src/main/java/com/rubbertranslator/enumtype/HotKey.java
+++ Main/src/main/java/com/rubbertranslator/enumtype/HotKey.java
@@ -1,6 +1,61 @@
 package com.rubbertranslator.enumtype;
 
+/**
+ * 码表
+ */
 public class HotKey {
+    public static final int A = 65;
+    public static final int B = 66;
+    public static final int C = 67;
+    public static final int D = 68;
+    public static final int E = 69;
+    public static final int F = 70;
+    public static final int G = 71;
+    public static final int H = 72;
+    public static final int I = 73;
+    public static final int J = 74;
+    public static final int K = 75;
+    public static final int L = 76;
+    public static final int M = 77;
+    public static final int N = 78;
+    public static final int O = 79;
+    public static final int P = 80;
+    public static final int Q = 81;
+    public static final int R = 82;
+    public static final int S = 83;
+    public static final int T = 84;
+    public static final int U = 85;
+    public static final int V = 86;
+    public static final int W = 87;
+    public static final int X = 88;
+    public static final int Y = 89;
+    public static final int Z = 90;
+    public static final int NUM0 = 48;
+    public static final int NUM1 = 49;
+    public static final int NUM2 = 50;
+    public static final int NUM3 = 51;
+    public static final int NUM4 = 52;
+    public static final int NUM5 = 53;
+    public static final int NUM6 = 54;
+    public static final int NUM7 = 55;
+    public static final int NUM8 = 56;
+    public static final int NUM9 = 57;
+    public static final int STAR = 106;
+    public static final int ADD = 107;
+    public static final int ENTER = 108;
+    public static final int SBU = 109;
+    public static final int DOT = 110;
+    public static final int SLASH = 111;
+    public static final int F1 = 112;
+    public static final int F2 = 113;
+    public static final int F3 = 114;
+    public static final int F4 = 115;
     public static final int F5 = 116;
     public static final int F6 = 117;
+    public static final int F7 = 118;
+    public static final int F8 = 119;
+    public static final int F9 = 120;
+    public static final int F10 = 121;
+    public static final int F11 = 122;
+    public static final int F12 = 123;
 }
